2d8f3f0c53ef28d763810a762e92468ac18802ef
Odysseas Stavrou <odysseassu@gmail.com>
2021-12-10 00:37:25 +0200

    Implement export functionality
    Also now send a CLEAR command before downloading the board

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use log::info;
+use log::{ info, error };
 use colored::*;
 use std::thread;
 use sudoku::Sudoku;
 use std::time::Duration;
-use std::fs::OpenOptions;
+use chrono::Local;
+use std::fs::{ OpenOptions, create_dir };
 use std::path::{ PathBuf };
 use strum::IntoEnumIterator;
 use rand::{ thread_rng, Rng };
@@ -240,6 +241,34 @@ impl SudokuAvr {
         return board;
     }
 
+    pub fn export_board(&self) -> Result<()> {
+        let dir = "exports";
+        match create_dir(dir) {
+            Ok(_) => (),
+            Err(e) => {
+                match e.kind() { 
+                    ErrorKind::AlreadyExists => (),
+                    _ => {
+                        error!("Unable to Create directory!");
+                        bail!("{}", format!("{:#}", e));
+                    }
+                }
+            } 
+        }
+
+        let filename = format!("{}_{}.txt", self.dif, Local::now().format("%d%m%Y_%H%M%S"));
+        let path = PathBuf::from(format!("./{}", dir)).join(filename.clone());
+
+        let mut f = OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .open(&path)
+                    .with_context(|| format!("Failed to create {}", path.display()))?;
+        
+        write!(f, "{}\n{}", self.dif, self.to_string())?;
+        info!("{}", format!("{}: {}", filename, "Exported Successfully"));
+        Ok(())
+    }
 
     // returns a String representation of the 9x9 Array
     // 0,0 -> 1st, 0,1 -> 2nd etc
